a295bbeb1345823db3f1b2c9d8f5d789a7471f2e
Iram Anwar <iram.raza23@gmail.com>
2024-11-28 20:44:46 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -3,14 +3,14 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-    const metadata = data.metadata;
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-    const resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    const result = resultArray[0];
+    let resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    let result = resultArray[0];
 
     // Use d3 to select the panel with id of `#sample-metadata`
-    const panel = d3.select("#sample-metadata");
+    let panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
     panel.html("");
@@ -28,19 +28,19 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-    const samples = data.samples;
+    let samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-    const resultArray = samples.filter(sampleObj => sampleObj.id == sample);
-    const result = resultArray[0];
+    let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    let result = resultArray[0];
 
     // Get the otu_ids, otu_labels, and sample_values
-    const otu_ids = result.otu_ids;
-    const otu_labels = result.otu_labels;
-    const sample_values = result.sample_values;
+    let otu_ids = result.otu_ids;
+    let otu_labels = result.otu_labels;
+    let sample_values = result.sample_values;
 
     // Build a Bubble Chart
-    const bubbleData = [
+    let bubbleData = [
       {
         x: otu_ids,
         y: sample_values,
@@ -53,7 +53,7 @@ function buildCharts(sample) {
         }
       }
     ];
-    const bubbleLayout = {
+    let bubbleLayout = {
       title: "Bacteria Cultures Per Sample",
       margin: { t: 30, l: 50 },
       xaxis: { title: "OTU ID" },
@@ -65,8 +65,8 @@ function buildCharts(sample) {
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
 
-    const yticks = otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
-    const barData = [
+    let yticks = otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
+    let barData = [
       {
         x: sample_values.slice(0, 10).reverse(),
         y: yticks,
@@ -77,7 +77,7 @@ function buildCharts(sample) {
     ];
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
-    const barLayout = {
+    let barLayout = {
       title: "Top 10 Bacteria Cultures Found",
       margin: { t: 30, l: 150 }
     };
@@ -93,10 +93,10 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-    const sampleNames = data.names;
+    let sampleNames = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-    const selector = d3.select("#selDataset");
+    let selector = d3.select("#selDataset");
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
@@ -106,7 +106,11 @@ function init() {
     });
 
     // Get the first sample from the list
+<<<<<<< HEAD:static/js/app.js
     const firstSample = sampleNames[0];
+=======
+    let firstSample = sampleNames[0];
+>>>>>>> bcbca25 (last commit):Starter_Code/static/js/app.js
     console.log(firstSample);
 
     // Build charts and metadata panel with the first sample
